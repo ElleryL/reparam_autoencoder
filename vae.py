4a022553fb0bd393ea30c282c63dfa47c4a9fec7
Ellery Lai <ellery@inside-65-106-213.wireless.utoronto.ca>
2018-06-08 11:18:50 -0400

    bug fix

--- vae.py
+++ vae.py
@@ -12,7 +12,7 @@ import torch.distributions as dist
 # Load MNIST and Set Up Data
 N = 500
 D = 784
-S = 50 # sample size for variational params
+
 N_data, train_images, train_labels, test_images, test_labels = load_mnist()
 train_images = torch.from_numpy(np.round(train_images[0:N])).float()
 train_labels = torch.from_numpy(train_labels[0:N]).float()
@@ -50,8 +50,8 @@ class VAE(nn.Module):
         :param log_std: log_std from encoding
         :return:
         '''
-        #eps = torch.FloatTensor(log_std.size()).normal_(0, 1)
-        eps = torch.ones([S,log_std.size()[0],log_std.size()[1]]).normal_(0, 1)
+        eps = torch.FloatTensor(log_std.size()).normal_(0, 1)
+
         std = torch.exp(log_std)
         z_tilde = mu + std*eps
         return z_tilde
@@ -69,42 +69,26 @@ class VAE(nn.Module):
 
         return z_tilde,mu,log_std
 
-    def log_Q_z_given_x(self,z_tilde,mu,log_std):
-        normal = torch.distributions.Normal(mu, torch.exp(log_std)**2)
-        log_q_z_given_x = torch.mean(torch.sum(normal.log_prob(z_tilde),dim=2),dim=1)
-
-
-        return log_q_z_given_x
-
-    def prior_log_p_z(self,z):
-        normal = dist.Normal(0,1)
-        return torch.mean(normal.log_prob(z.mean(0)),dim=1)
-
 
     def bernoulli_decode(self, z_tilde):
         h1 = self.Tanh(self.layer21(z_tilde))
         y = self.Sig(self.layer22(h1))
         return y
 
-    def log_P_x_given_z(self,y,x):
+    def Recon_Loss(self, y, x):
         return F.binary_cross_entropy(y,x,size_average=False)
 
     def forward(self, x):
 
         z_tilde,mu,log_std = self.encode(x)
-        y = self.bernoulli_decode(z_tilde.mean(0))
+        y = self.bernoulli_decode(z_tilde)
 
         return y,z_tilde,mu,log_std
 
-    def objective_func(self, z_tilde,log_p_x_given_z,mu,log_std):
+    def objective_func(self,recon_loss,mu,log_std):
 
         KL = -0.5 * torch.sum(1 + log_std - (mu ** 2) - torch.exp(log_std))
-        neg_elbo = (KL + log_p_x_given_z)
-
-        log_q_z_given_x = self.log_Q_z_given_x(z_tilde,mu,log_std)
-
-        neg_elbo = ((log_p_x_given_z + self.prior_log_p_z(z_tilde).mean()) - log_q_z_given_x.mean())
-
+        neg_elbo = (KL + recon_loss)
 
         return neg_elbo
 
@@ -121,8 +105,8 @@ def train_vae(vae,opt,iters,batch_size,dataX,dataY):
             batch_x, batch_y = dataX[indices], dataY[indices]
 
             y, z_tilde,mu,log_std= vae(batch_x)
-            log_p_x_given_z = vae.log_P_x_given_z(y,batch_x)
-            loss = vae.objective_func(z_tilde,log_p_x_given_z,mu,log_std)
+            recon_loss = vae.Recon_Loss(y, batch_x)
+            loss = vae.objective_func(recon_loss,mu,log_std)
 
             train_loss += loss
             loss.backward()
@@ -156,9 +140,9 @@ np.random.seed(14)
 vae = VAE(D,400,20)
 opt = optim.Adam(vae.parameters(), lr=1e-3)
 
-loss_curve = train_vae(vae,opt,200,50,train_images,train_labels)
+loss_curve = train_vae(vae,opt,1000,50,train_images,train_labels)
 
-# we do some simple Turing test here
+# we do some simple test here
 simulateImage(test_images[:10],vae)
 
 
